f1b747faa370350a75ef3910c4a9622761f315ab
omar96developer <omakino@hotmail.it>
2022-04-20 05:23:09 +0200

    inizialized milestone 2

---
+++ milestone-2/script.js
@@ -0,0 +1,22 @@
+/* $(document).ready(function () {
+  $('#title').mouseenter(function (event) {
+    console.log(event);
+  });
+
+  $('input[name="name"]').click(clickInput);
+
+
+  $('.list-element').click(function () {
+    console.log(this);
+    $(this).hide();
+  });
+
+  $('#title').click(() => {
+    console.log(this);
+  });
+
+});
+
+function clickInput() {
+  console.log("click input");
+} */
